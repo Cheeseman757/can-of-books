119fce44179f08ac4058d169b9d56d046a3ed603
Justin Cheeseman <156013482+Cheeseman757@users.noreply.github.com>
2024-03-11 21:59:04 -0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+function Home() {
+  return (
+    <div>
+      <h2>Home Page</h2>
+      {/* Add content for the home page */}
+    </div>
+  );
+}
+
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+export default main;
