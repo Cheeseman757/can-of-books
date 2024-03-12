1f0c69ec81e8e6d8610381d68d09f63abdcb3067
Justin Cheeseman <156013482+Cheeseman757@users.noreply.github.com>
2024-03-12 07:02:01 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,105 @@
-import React from 'react';
-import Header from './Header';
-import Footer from './Footer';
-import BestBooks from './BestBooks';
-import About from './About';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from "react-router-dom";
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import axios from 'axios';
+import { Carousel } from 'react-bootstrap';
+
+class BestBooks extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      books: [],
+    };
+  }
+
+  componentDidMount() {
+    // Make a GET request to the server's /books route
+    axios.get('/books')
+      .then(response => {
+        // Store the book data in the application state
+        this.setState({ books: response.data });
+      })
+      .catch(error => {
+        console.error('Error fetching books:', error);
+      });
+  }
+
+  renderBooks() {
+    // Check if there are more than 0 books stored in the application state
+    if (this.state.books.length > 0) {
+      // Render Bootstrap Carousel with books
+      return (
+        <Carousel>
+          {this.state.books.map(book => (
+            <Carousel.Item key={book.id}>
+              <img
+                className="d-block w-100"
+                src={book.imageUrl}
+                alt={book.title}
+              />
+              <Carousel.Caption>
+                <h3>{book.title}</h3>
+                <p>{book.author}</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+          ))}
+        </Carousel>
+      );
+    } else {
+      // Render message when no books are available
+      return <p>Book collection is empty.</p>;
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        {this.renderBooks()}
+      </div>
+    );
+  }
+}
+
+class About extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>About</h2>
+        <p>Project developed by Kawika & Justin</p>
+        <p>
+          Find me on GitHub: <a href="https://github.com/your-github-username">GitHub</a>
+        </p>
+      </div>
+    );
+  }
+}
 
 class App extends React.Component {
   render() {
     return (
-      <>
-        <nav>
-        </nav>
-        <Router>
-          <Header />
-          <Routes>
-            <Route 
-              exact path="/"
-              element={<BestBooks />}
-            >
-            </Route>
-            <Route 
-              exact path="/about"
-              element={<About />}
-            >
-            </Route>
-          </Routes>
-          <Footer />
-        </Router>
-      </>
-    )
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <hr />
+
+          <Route exact path="/" component={BestBooks} />
+          <Route path="/about" component={About} />
+        </div>
+      </Router>
+    );
   }
 }
 
 export default App;
-
 
 
 
